4cca6765b7240944e33c4269032a33c3e6bca867
v3rena <v.poetzl@gmx.at>
2017-05-24 11:49:38 +0200

    Result Details added, refactored

--- app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ProductDetailActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ProductDetailActivity.java
@@ -1,13 +1,10 @@
 package com.google.android.gms.samples.vision.barcodereader;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.JsonReader;
 import android.util.JsonToken;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.barcode.Barcode;
@@ -19,32 +16,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static com.google.android.gms.samples.vision.barcodereader.BarcodeCaptureActivity.BarcodeObject;
-public class BarcodeResultActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView PalmOilResult;
-    private ImageView ResultImage;
-    private Barcode barcode;
+public class ProductDetailActivity extends AppCompatActivity {
+
+    private TextView ProductDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barcode_result);
-        barcode = getIntent().getParcelableExtra(BarcodeObject);
-        PalmOilResult = (TextView)findViewById(R.id.palm_oil_result);
-        ResultImage = (ImageView)findViewById(R.id.result_image);
-        findViewById(R.id.show_details).setOnClickListener(this);
-
+        setContentView(R.layout.activity_product_detail);
+        Barcode barcode = getIntent().getParcelableExtra(BarcodeObject);
         String url = "https://world.openfoodfacts.org/api/v0/product/" + barcode.rawValue + ".json";
         new JsonTask().execute(url);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.show_details) {
-            Intent intent = new Intent(this, ProductDetailActivity.class);
-            intent.putExtra(BarcodeObject, barcode);
-            startActivity(intent);
-        }
     }
 
     private class JsonTask extends AsyncTask<String, String, InputStream> {
@@ -78,14 +61,13 @@ public class BarcodeResultActivity extends AppCompatActivity implements View.OnC
 
         private void readJSON(InputStream inputStream) throws IOException {
             try (JsonReader reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"))) {
-                int resultCode = parseJSON(reader);
+                parseJSON(reader);
                 reader.close();
-                setResult(resultCode);
             }
         }
 
-        private int parseJSON(JsonReader reader) throws  IOException {
-            int resultCode = 999;
+        private void parseJSON(JsonReader reader) throws  IOException {
+            int resultCode = 0;
             reader.beginObject();
             loop:
             while (reader.hasNext()) {
@@ -110,13 +92,11 @@ public class BarcodeResultActivity extends AppCompatActivity implements View.OnC
                                     String name = reader.nextName();
                                     if (name.contains("palm_oil")) {
                                         if (reader.peek().equals(JsonToken.NUMBER)) {
-                                            int productsFromPalmOil = reader.nextInt();
-                                            if (productsFromPalmOil == 1) {
-                                                resultCode = 1;
+                                            resultCode = reader.nextInt();
+                                            if (resultCode == 1) {
+                                                //reader.endObject();
+                                                //return resultCode;
                                                 break;
-                                            }
-                                            else if (productsFromPalmOil == 0) {
-                                                resultCode = 0;
                                             }
                                         } else {
                                             reader.skipValue();
@@ -126,13 +106,10 @@ public class BarcodeResultActivity extends AppCompatActivity implements View.OnC
                                             String ingredients = reader.nextString();
                                             if ((ingredients.contains("palm oil")) || (ingredients.contains("palm-oil")) || (ingredients.contains("palm_oil")) || (ingredients.contains("Palmöl"))) {
                                                 resultCode = 1;
+                                                //reader.endObject();
+                                                //return resultCode;
                                                 break;
                                             }
-                                            else {
-                                                resultCode = 0;
-                                            }
-                                        } else if (reader.peek().equals((JsonToken.NULL))) {
-                                            resultCode = 206;
                                         } else {
                                             reader.skipValue();
                                         }
@@ -152,53 +129,7 @@ public class BarcodeResultActivity extends AppCompatActivity implements View.OnC
                 }
             }
             //reader.endObject();
-            return resultCode;
-        }
-
-        private void setResult(int resultCode) {
-            switch(resultCode){
-                case 0:
-                    setResultContainsPalmOil();
-                    break;
-                case 1:
-                    setResultPalmOilFree();
-                    break;
-                case 404:
-                    setResultProductNotFound();
-                    break;
-                case 206:
-                    setResultInfoNotAvailable();
-                    break;
-                default:
-                    setResultError();
-            }
-        }
-
-        private void setResultContainsPalmOil() {
-            PalmOilResult.setText(R.string.result_text_palm_oil_free);
-            ResultImage.setImageResource(R.drawable.check);
-        }
-
-        private void setResultPalmOilFree() {
-            PalmOilResult.setText(R.string.result_contains_palm_oil);
-            ResultImage.setImageResource(R.drawable.cross);
-        }
-
-        private void setResultProductNotFound() {
-            PalmOilResult.setText(R.string.result_product_not_found);
-            ResultImage.setImageResource(R.drawable.search);
-        }
-
-        private void setResultInfoNotAvailable() {
-            PalmOilResult.setText(R.string.result_text_info_not_available);
-            ResultImage.setImageResource(R.drawable.wrench);
-        }
-
-        private void setResultError() {
-            PalmOilResult.setText(R.string.result_technical_problems);
-            ResultImage.setImageResource(R.drawable.wrench);
         }
     }
 }
-
 
